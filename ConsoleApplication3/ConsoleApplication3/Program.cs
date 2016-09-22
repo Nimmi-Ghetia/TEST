27c3dd39895c472a98059229e74fd17d5cf20199
Nimmi-Ghetia <14ceuog068.ce@ddu.ac.in>
2016-09-22 12:49:47 +0530

    this is 3rd prgm

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    public static class MyExtension
+    {
+        public static int CountWord(this string st)
+        {
+            int Count=0;
+            for (int i = 0; i < st.Length; i++)
+            {
+                if (st[i] == ' ')
+                {
+                    Count++;
+                }
+            }
+            return Count;
+        }
+        public static int CountCharacter(this string st)
+        {
+  //          int Count = 0;
+           return st.Length;
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter statement:");
+            string st = Console.ReadLine();
+            int words = st.CountWord();
+            int characters = st.CountCharacter();
+            Console.WriteLine("words:" + words);
+            Console.WriteLine("characters:" + characters);
+            Console.ReadLine();
+        }
+    }
+}
